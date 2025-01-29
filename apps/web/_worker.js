0a65f5964698093ba53e827d32b72c883f7ec22d
Camillo Bovio <camillo@18tre.com>
2025-01-29 13:08:52 +0100

    test header

---
+++ apps/web/_worker.js
@@ -0,0 +1,16 @@
+export default {
+  async fetch(request, env, ctx) {
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        status: 204,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+          'Access-Control-Max-Age': '86400',
+        },
+      });
+    }
+    return env.ASSETS.fetch(request);
+  },
+};
