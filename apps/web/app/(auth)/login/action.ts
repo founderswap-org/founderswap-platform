184eddf19af5d628642ee6bdeb5395bace68e7c8
Camillo Bovio <camillo@18tre.com>
2025-01-30 09:52:06 +0100

    removed comments

--- apps/web/app/(auth)/login/action.ts
+++ apps/web/app/(auth)/login/action.ts
@@ -29,8 +29,6 @@ export async function login(formData: FormData) {
 export async function signup(formData: FormData) {
   const supabase = await createClient()
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data: SignUpWithPasswordCredentials = {
     email: formData.get('email') as string,
     password: formData.get('password') as string,
