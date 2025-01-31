cd4bfac79ecb77db002fb1bd6d7daa0fbdde84e4
Camillo Bovio <camillo@18tre.com>
2025-01-31 13:37:01 +0100

    removed fragment

--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -4,13 +4,11 @@ import { LoginForm } from '@/components/auth/login-form';
 
 export default function LoginPage() {
   return (
-    <>
-      <AuthCard
-        title="Welcome back on board!"
-        description="Let’s get you back to exploring."
-      >
-        <LoginForm />
-      </AuthCard>
-    </>
+    <AuthCard
+      title="Welcome back on board!"
+      description="Let’s get you back to exploring."
+    >
+      <LoginForm />
+    </AuthCard>
   );
 }
