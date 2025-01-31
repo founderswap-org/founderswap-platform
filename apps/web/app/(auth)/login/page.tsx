fde9d25254d2b4422b4d75ed311a301e39df09f4
Camillo Bovio <camillo@18tre.com>
2025-01-31 13:29:33 +0100

    fix: removed separator from login page

--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -1,13 +1,10 @@
-'use client';
 export const runtime = 'edge';
 import { AuthCard } from '@/components/auth/card';
 import { LoginForm } from '@/components/auth/login-form';
-import { Separator } from '@founderswap/design-system/components/ui/separator';
 
 export default function LoginPage() {
   return (
     <>
-      <Separator />
       <AuthCard
         title="Welcome back on board!"
         description="Let’s get you back to exploring."
